29b22a7a92084a8ce87be6a6c387093f08d20546
Fabian Fulga <ffulga@cloudbasesolutions.com>
2025-04-01 12:47:48 +0300

    Add tests for netpreserver

---
+++ coriolis/tests/osmorphing/netpreserver/test_netplan.py
@@ -0,0 +1,99 @@
+# Copyright 2025 Cloudbase Solutions Srl
+# All Rights Reserved.
+
+from unittest import mock
+
+from coriolis.osmorphing import debian
+from coriolis.osmorphing.netpreserver import netplan
+from coriolis.tests import test_base
+
+
+class NetplanNetPreserverTestCase(test_base.CoriolisBaseTestCase):
+    """Test suite for the NetplanNetPreserver class."""
+
+    def setUp(self):
+        super(NetplanNetPreserverTestCase, self).setUp()
+        self.event_manager = mock.MagicMock()
+        self.detected_os_info = {
+            'os_type': 'linux',
+            "release_version": '10',
+            'distribution_name': debian.DEBIAN_DISTRO_IDENTIFIER,
+            'friendly_release_name': mock.sentinel.friendly_release_name,
+        }
+        self.netpreserver = netplan.NetplanNetPreserver(
+            debian.BaseDebianMorphingTools(
+                mock.sentinel.conn,
+                mock.sentinel.os_root_dir,
+                mock.sentinel.os_root_dir,
+                mock.sentinel.hypervisor,
+                self.event_manager,
+                self.detected_os_info,
+                mock.sentinel.osmorphing_parameters,
+                mock.sentinel.operation_timeout
+            )
+        )
+
+    @mock.patch.object(debian.BaseDebianMorphingTools, '_list_dir')
+    @mock.patch.object(debian.BaseDebianMorphingTools, '_test_path')
+    def test_check_net_preserver_true(self, mock_test_path, mock_list_dir):
+        mock_test_path.return_value = True
+        mock_list_dir.return_value = ["01-netplan.yaml", "notconfig.txt",
+                                      "02-other.yml"]
+
+        result = self.netpreserver.check_net_preserver()
+
+        mock_test_path.assert_called_once_with(self.netpreserver.netplan_base)
+        mock_list_dir.assert_called_once_with(self.netpreserver.netplan_base)
+        self.assertTrue(result)
+
+    @mock.patch.object(debian.BaseDebianMorphingTools, '_list_dir')
+    @mock.patch.object(debian.BaseDebianMorphingTools, '_test_path')
+    def test_check_net_preserver_false_due_to_non_yaml(self, mock_test_path,
+                                                       mock_list_dir):
+        mock_test_path.return_value = True
+        mock_list_dir.return_value = ["config.txt", "README.md"]
+
+        result = self.netpreserver.check_net_preserver()
+
+        self.assertFalse(result)
+
+    @mock.patch.object(debian.BaseDebianMorphingTools, '_read_file_sudo')
+    @mock.patch.object(debian.BaseDebianMorphingTools, '_list_dir')
+    def test_parse_network(self, mock_list_dir, mock_read_file_sudo):
+        mock_list_dir.return_value = ["01-netplan.yaml"]
+        yaml_content = """
+            network:
+            ethernets:
+                eth0:
+                    match:
+                        macaddress: "00:11:22:33:44:55"
+                    addresses:
+                        - "192.168.1.10/24"
+                eth1:
+                    addresses:
+                        - "192.168.1.20/24"
+                        - "192.168.1.21/24"
+                eth2:
+                    match:
+                        macaddress: "AA:BB:CC:DD:EE:FF"
+        """
+        mock_read_file_sudo.return_value = yaml_content.lstrip().encode()
+
+        self.netpreserver.parse_network()
+
+        expected_info = {
+            "eth0": {
+                "mac_address": "00:11:22:33:44:55",
+                "ip_addresses": ["192.168.1.10"]
+            },
+            "eth1": {
+                "mac_address": "",
+                "ip_addresses": ["192.168.1.20", "192.168.1.21"]
+            },
+            "eth2": {
+                "mac_address": "AA:BB:CC:DD:EE:FF",
+                "ip_addresses": []
+            }
+        }
+
+        self.assertEqual(self.netpreserver.interface_info, expected_info)
